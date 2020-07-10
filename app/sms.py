1198ac069c4b18c0b6882a20b5df45b35173a021
adowie <adowie01@gmail.com>
2020-07-10 16:15:54 -0500

    add about page

--- app/sms.py
+++ app/sms.py
@@ -5,9 +5,7 @@ import sys
 account_sid = os.getenv('SMS_SID')
 auth_token = os.getenv('SMS_AUTH')
 
-client = TextmagicRestClient(account_sid, auth_token)
-to = sys.argv[1]
-msg = sys.argv[2]
-message = client.messages.create(phones=to, text=msg)
+def sms(to,msg):
+	client = TextmagicRestClient(account_sid, auth_token)
+	message = client.messages.create(phones=to, text=msg)
 
-print(message)
